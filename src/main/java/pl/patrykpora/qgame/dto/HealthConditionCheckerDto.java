9002baf4a403503961f3cdff090e8eb064eb711a
unknown <patryk_pora@o2.pl>
2022-06-03 09:21:32 +0200

    thymeleaf with hellocontroller

--- src/main/java/pl/patrykpora/qgame/dto/HealthConditionCheckerDto.java
+++ src/main/java/pl/patrykpora/qgame/dto/HealthConditionCheckerDto.java
@@ -1,22 +1,15 @@
 package pl.patrykpora.qgame.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class HealthConditionCheckerDto {
 
     private boolean status;
     private String message;
-
-    public HealthConditionCheckerDto(boolean status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 
 
 }
